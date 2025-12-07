ee26c592bf46b3253cbec67a27c1b0a5ad3554ad
thomas <thomasnuernberg@icloud.com>
2025-12-07 22:14:30 +0100

    .

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -3,7 +3,7 @@ package h1;
 
 public class H1_main {
     public static void main(String[] args) {
-        Bruch bruch = new Bruch(16,4);
+        Bruch bruch = new Bruch(-16,-4);
         Bruch bruchee = new Bruch(32,8);
         System.out.println(bruch.zaehler);
         System.out.println(bruch.nenner);
